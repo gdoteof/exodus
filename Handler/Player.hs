34b6230bfbfa772180f2611aff586a715a94e267
Geoff Golder <gdoteof@gmail.com>
2012-03-12 16:54:16 -0600

    initial commit

---
+++ Handler/Player.hs
@@ -0,0 +1,54 @@
+module Handler.Player
+    ( getPlayerListR
+    , postPlayerListR
+    , getPlayerR
+    )
+where
+
+
+import Import 
+
+-- to use Html into forms
+import Yesod.Form.Nic (YesodNic, nicHtmlField)
+import Data.Maybe
+
+playerForm :: Form Player
+playerForm = renderDivs $ Player
+    <$> areq   textField "Name" Nothing
+    <*> areq   textField "Nick" Nothing
+    <*> aopt   textField "Email" Nothing
+    <*> aopt   textField "Phone" Nothing
+    <*> aopt   textareaField "Notes" Nothing
+
+getPlayerListR :: Handler RepHtml
+getPlayerListR = do
+    defaultLayout $ do
+        [whamlet|<h1>iget|]
+    players <- runDB $ selectList [] [Desc PlayerName]
+    -- We'll need the two "objects": articleWidget and enctype
+    -- to construct the form (see templates/articles.hamlet).
+    ((_,playerWidget), enctype) <- generateFormPost playerForm
+    defaultLayout $ do
+        setTitle "Player List"
+        $(widgetFile "players")
+
+
+postPlayerListR :: Handler RepHtml
+postPlayerListR = do
+    ((res,playerWidget),enctype) <- runFormPost playerForm
+    case res of 
+         FormSuccess player -> do 
+            playerId <- runDB $ insert player
+            setMessage $ toHtml $ (playerName player) <> " created"
+            redirect $ PlayerR playerId 
+         _ -> defaultLayout $ do
+                setTitle "Please correct your entry form"
+                $(widgetFile "playerAddError")
+
+
+getPlayerR :: PlayerId -> Handler RepHtml
+getPlayerR playerId = do
+     player <- runDB (get404 playerId)
+     defaultLayout $ do 
+                   setTitle "Testing" 
+                   $(widgetFile "player")
