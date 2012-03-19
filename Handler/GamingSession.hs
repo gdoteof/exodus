42c5ee09f09dcb718fa3bdfd79bfe5182c03faaa
Geoff Golder <gdoteof@gmail.com>
2012-03-19 00:01:31 -0600

    updated GamingSession handler

--- Handler/GamingSession.hs
+++ Handler/GamingSession.hs
@@ -1,19 +1,42 @@
 module Handler.GamingSession
     ( postGamingSessionR
-    , getGamingSessionsR
+    , getGamingSessionR
     )
 where
 
+import Import
+import Data.Time.Clock
+import Database.Persist.Store
+import Data.Text
+import Data.Maybe
+
 postGamingSessionR :: Handler RepHtml
 postGamingSessionR = do
+    start <- liftIO $ getCurrentTime
     gs <- runInputPost $ GamingSession
-                do { start <- getCurrentTime }
+                start
                 Nothing
-                <$> ireq textField "player"
-                <*> ireq textField "table"
-                <*> ireq intField "seat"
-    defaultLayout [whamlet|<p>#{show person}|]
+                <$> (textToKey <$> (ireq textField "player"))
+                <*> (textToKey <$> (ireq textField "table"))
+                <*> iopt intField "seat"
+    let _ = gs :: GamingSession
+    gsId <- runDB $ insert gs
+    defaultLayout $(widgetFile "newSession.hamlet")
   
+textToKey = Key . PersistInt64 . read . unpack
+--textToKey = maybe (fail "bad input") return .  fromPathPiece
+
+
 getGamingSessionR :: Handler RepHtml
 getGamingSessionR = do
-  defaultLayout [whamlet |<h1>Hi]
+  defaultLayout [whamlet|
+<form action=@{GamingSessionR} method=post>
+    <p>
+        player
+        <input type=text name=player value=4f6150251c21230c78000000>
+        table
+        <input type=text name=table value=4f651bfa1c21231873000000>
+        table
+        <input type=text name=seat value=2>
+        <input type=submit value="make">
+|]
